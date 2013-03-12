ac7252eca67d4951f0cdd57d1c6ae0adac4f29bd
whatknows <whatknows@gmail.com>
2013-03-12 16:49:53 -0700

    Added Album module
    This also includes some other little changes that are confusing.

--- fbdeath/public/index.php
+++ fbdeath/public/index.php
@@ -1,25 +1,12 @@
 <?php
-// Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
 
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+// Setup autoloading
+require 'init_autoloader.php';
 
-// Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
-
-/** Zend_Application */
-require_once 'Zend/Application.php';
-
-// Create application, bootstrap, and run
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
-$application->bootstrap()
-            ->run();
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();

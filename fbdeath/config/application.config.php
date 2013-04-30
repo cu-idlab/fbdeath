a9222fd35d93df4bcaa1e6271baa16f3b7a6b580
whatknows <whatknows@gmail.com>
2013-04-30 07:37:41 -0700

    Messing around with UserAuth module -- trying to get the override to work. No success yet.

--- fbdeath/config/application.config.php
+++ fbdeath/config/application.config.php
@@ -3,10 +3,11 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
     	'Application',
-        'Album',
+        'ScnSocialAuth',
         'ZfcBase',
         'ZfcUser',
-        'ScnSocialAuth'
+        'Album',
+        'User'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
---
+++ fbdeath/module/User/Module.php
@@ -0,0 +1,10 @@
+<?php
+namespace User;
+
+class Module
+{
+	public function init($moduleManager)
+	{
+		$moduleManager->loadModule('ZfcUser');
+	}
+}
